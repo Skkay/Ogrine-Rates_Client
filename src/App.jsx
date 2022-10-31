9d06cbed996e32bf738da50c81d219d78a93740e
Skkay <skkay.thomas@gmail.com>
2022-10-31 11:21:28 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <h1 className="text-3xl font-bold underline">
+        Ogrine Rates
+      </h1>
+    </div>
+  )
+}
+
+export default App;
