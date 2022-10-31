8ac86a7a330a48364d7e3c60866a4e1ddcd660a9
Skkay <skkay.thomas@gmail.com>
2022-10-31 16:43:07 +0100

    feat(component): Add Card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import { PropTypes } from 'prop-types';
+
+const Card = ({ title, content }) => {
+  return (
+    <div className="bg-white w-96 rounded-lg shadow-md m-3 p-3">
+      <p className="text-gray-500 mb-1">{title}</p>
+      {content}
+    </div>
+  )
+}
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.any.isRequired,
+}
+
+export default Card;
