859491f94f94ea900016d39b38ed0cdafb107ea1
Skkay <skkay.thomas@gmail.com>
2022-11-01 19:20:17 +0100

    feat(fetch): Add 'fetchOgrineRates' function

---
+++ src/fetch/fetchOgrineRate.jsx
@@ -0,0 +1,25 @@
+import { OGRINE_API_URL, fetchOptions } from './options';
+
+const fetchOgrineRates = (limit = 0, sort = 'DESC') => {
+  const params = {
+    sort,
+    limit,
+  };
+
+  const query = Object.keys(params)
+    .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+    .join('&')
+  ;
+
+  const init = {
+    ...fetchOptions,
+    method: 'GET',
+  };
+
+  return fetch(`${OGRINE_API_URL}/ogrineRates?${query}`, init)
+    .then((res) => res.json())
+    .catch((err) => console.error(err))
+  ;
+}
+
+export { fetchOgrineRates };
---
+++ src/fetch/options.jsx
@@ -0,0 +1,10 @@
+const OGRINE_API_URL = import.meta.env.VITE_OGRINE_API_URL;
+
+const fetchOptions = {
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
+}
+
+export { OGRINE_API_URL, fetchOptions };
