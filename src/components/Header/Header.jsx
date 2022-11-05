a310937614055a4bf821ad385a6612416648d983
Skkay <skkay.thomas@gmail.com>
2022-11-05 11:37:31 +0100

    style: Set tab size to 4 spaces

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,28 +3,28 @@ import RateCardDetail from "../CardDetail/Rate/RateCardDetail";
 import ogrine from '../../assets/ogrine.svg'
 
 const Header = () => {
-  return (
-    <div className="flex items-center bg-white shadow-md rounded-lg">
-      <div className="flex items-center">
-        <div className="flex w-[62px] h-[62px] bg-shadow bg-center mx-3">
-          <img className="m-auto w-10" src={ogrine} alt="" />
+    return (
+        <div className="flex items-center bg-white shadow-md rounded-lg">
+            <div className="flex items-center">
+                <div className="flex w-[62px] h-[62px] bg-shadow bg-center mx-3">
+                    <img className="m-auto w-10" src={ogrine} alt="" />
+                </div>
+                <h1 className="text-3xl">Taux de l'Ogrine</h1>
+            </div>
+            <div className="flex ml-auto">
+                <Card
+                    title="Moyenne des 7 derniers jours"
+                    content={
+                        <RateCardDetail value={505.6} />
+                    } />
+                <Card
+                    title="Taux du jour"
+                    content={
+                        <RateCardDetail value={505.6} percent={25.2} />
+                    } />
+            </div>
         </div>
-        <h1 className="text-3xl">Taux de l'Ogrine</h1>
-      </div>
-      <div className="flex ml-auto">
-        <Card
-          title="Moyenne des 7 derniers jours"
-          content={
-            <RateCardDetail value={505.6} />
-          } />
-        <Card
-          title="Taux du jour"
-          content={
-            <RateCardDetail value={505.6} percent={25.2} />
-          } />
-        </div>
-    </div>
-  );
+    );
 }
 
 export default Header;
--- src/fetch/fetchOgrineRate.jsx
+++ src/fetch/fetchOgrineRate.jsx
@@ -1,25 +1,25 @@
 import { OGRINE_API_URL, fetchOptions } from './options';
 
 const fetchOgrineRates = (limit = 0, sort = 'ASC') => {
-  const params = {
-    sort,
-    limit,
-  };
+    const params = {
+        sort,
+        limit,
+    };
 
-  const query = Object.keys(params)
-    .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-    .join('&')
-  ;
+    const query = Object.keys(params)
+        .map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+        .join('&')
+    ;
 
-  const init = {
-    ...fetchOptions,
-    method: 'GET',
-  };
+    const init = {
+        ...fetchOptions,
+        method: 'GET',
+    };
 
-  return fetch(`${OGRINE_API_URL}/ogrineRates?${query}`, init)
-    .then((res) => res.json())
-    .catch((err) => console.error(err))
-  ;
+    return fetch(`${OGRINE_API_URL}/ogrineRates?${query}`, init)
+        .then((res) => res.json())
+        .catch((err) => console.error(err))
+    ;
 }
 
 export { fetchOgrineRates };
